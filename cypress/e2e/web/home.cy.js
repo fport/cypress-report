1e33be86bd176d50992b5cbc0c78122331e647d9
Furkan Portakal <furkan.portakal@demirorenteknoloji.com>
2023-02-09 14:01:39 +0300

    add basic test for actions

--- cypress/e2e/web/home.cy.js
+++ cypress/e2e/web/home.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
 describe('My First Test', () => {
-    it('Does not do much!', () => {
-        expect(true).to.equal(true)
+    context('Pass Test', () => {
+        it('Does not do much!', () => {
+            expect(true).to.equal(true)
+        })
+    })
+    context('Fail Test', () => {
+        it('Does not do much!', () => {
+            expect(true).to.equal(false)
+        })
     })
 });

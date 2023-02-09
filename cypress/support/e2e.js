eff442e0da0812165e5526dd86c7d2670eb98e2f
Furkan Portakal <furkan.portakal@demirorenteknoloji.com>
2023-02-09 16:46:09 +0300

    fix

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,8 @@
 import './commands'
 import 'cypress-plugin-api'
+
+Cypress.on('uncaught: exception', (err, runnable) => {
+    if (err.message.includes('Cannot read properties of undefined')) {
+        return false
+    }
+})
